d727ecf9de7b612cd7f196e63a5cca8c1e985a3b
Fabinho15 <51960922+Fabinho15@users.noreply.github.com>
2021-04-22 21:29:35 -0300

    Update Item.cs

--- Item.cs
+++ Item.cs
@@ -9,13 +9,15 @@ public class Item
     public string Descricao;
     public int Florim;
     public int Qnt;
+    public string Categ;
 
-    public Item(string nome, string descricao, int florim, int qnt)
+    public Item(string nome, string descricao, int florim, int qnt, string categ)
     {
       this.Nome = nome;
       this.Descricao = descricao;
       this.Florim = florim;
       this.Qnt = qnt;
+      this.Categ = categ;
     }
     
     public void ImprimirItem()
@@ -23,6 +25,7 @@ public class Item
       Console.WriteLine("\r\nNome:\t\t{0}", this.Nome);
       Console.WriteLine("Descrição:\t{0}", this.Descricao);
       Console.WriteLine("Moedas:\t\t{0}", this.Florim);
-      Console.WriteLine("Quantidade:\t{0}\r\n", this.Qnt);
+      Console.WriteLine("Quantidade:\t{0}", this.Qnt);
+      Console.WriteLine("Categoria:\t{0}\r\n", this.Categ);
     }
   }
